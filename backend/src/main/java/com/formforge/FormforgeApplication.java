74cacf1eba1d9e3d041a5c2ac9aa8c0a4361e411
Andrei-Borta <andrei.borta@stud.ubbcluj.ro>
2026-04-18 16:53:04 +0300

    init backend

---
+++ backend/src/main/java/com/formforge/FormforgeApplication.java
@@ -0,0 +1,13 @@
+package com.formforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormforgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormforgeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/formforge/FormforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormforgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
